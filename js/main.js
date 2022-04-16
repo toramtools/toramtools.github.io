3534be519aee87c73c4378c7d20cd914f3733995
ToramTools <kaiba1947@hotmail.com>
2022-04-16 07:12:12 -0300

    feat(): new cap 245

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 "use strict"
 
-const LV_CAP = 240;
+const LV_CAP = 245;
 
-const HIGHEST_PROF = 205;
-const HIGHEST_ARM_POT = 46;
-const HIGHEST_WPN_POT = 47;
-const ARMOR_DIFFICULTY = 205;
-const WEAPON_DIFFICULTY = 200;
-const PRIMARY_STAT = 375;
+const HIGHEST_PROF = 220;
+const HIGHEST_ARM_POT = 49;
+const HIGHEST_WPN_POT = 49;
+const ARMOR_DIFFICULTY = 215;
+const WEAPON_DIFFICULTY = 210;
+const PRIMARY_STAT = 390;
 const SECONDARY_STAT = 247;
 
 const range = function (begin, end, step=1) {
